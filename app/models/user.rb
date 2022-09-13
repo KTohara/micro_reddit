5c0ac390d9c855d5d5493ceb5bc0980ef04d330c
kenta <ktohara@gmail.com>
2022-09-13 17:06:10 -0400

    Playing with validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,16 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  validates :username, presence: true, uniqueness: true, length: { in: 5..20 }
+  # validates :username, presence: true, uniqueness: true, length: { in: 5..20 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { in: 6..20 }
+
+  validate :name_validator
+
+  def name_validator
+    
+    unless username.length.between?(5, 20)
+      errors.add(:username, 'must be between 5 and 20 characters')
+    end
+  end
 end
